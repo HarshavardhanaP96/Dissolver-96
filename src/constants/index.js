62fbf301664df8a7e057ffe51a099dc7fe82b8e9
harshavardhanaP96 <pkhrao3112.harsha@gmail.com>
2024-08-15 17:14:56 +0530

    favicon update

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,11 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import shorturl from "../assets/projects/url_shortner.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with refined skills in front-end technologies like React and Next.js, and back-end technologies such as Node.js, MySQL, PostgreSQL, and MongoDB. I also utilize tools like Docker to enhance my development process. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = ``
-
+export const ABOUT_TEXT = ``;
 
 export const EXPERIENCES = [
   {
@@ -14,7 +14,14 @@ export const EXPERIENCES = [
     role: "Fullstack Developer Intern",
     company: "Grudhra Solution Pvt. Ltd.",
     description: `Creating admin panel and app for farming managemnt using Next.Js and React. Databse creation, backend route handlings using express, authetication and role based autherisation etc.`,
-    technologies: ["Javascript", "React.js", "Next.js", "Postgres", "Prisma","Express"],
+    technologies: [
+      "Javascript",
+      "React.js",
+      "Next.js",
+      "Postgres",
+      "Prisma",
+      "Express",
+    ],
   },
   {
     year: "2022 - 2024",
@@ -28,7 +35,7 @@ export const EXPERIENCES = [
     role: "Structural Analysis Intern",
     company: "Aviocian Tech",
     description: `Designed and anlysed components of the quadcoptors like frame, arm and legs using Msc. Patran, and Nastran solver. Prepared documentation and Analytical proof to match the resultus of the solver.`,
-    technologies: ["Msc.Patran", "Nastran","Solidworks"],
+    technologies: ["Msc.Patran", "Nastran", "Solidworks"],
   },
   {
     year: "2021 - 2021",
@@ -43,42 +50,51 @@ export const PROJECTS = [
   {
     title: "E-Commerce Website",
     image: project1,
-    reference:"https://www.google.com",                //to be updated
+    reference: "https://www.google.com", //to be updated
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication. admin panel and dashboard",
-    technologies: ["HTML", "Tailwind CSS", "React", "Node.js", "MongoDB", "Redux"],
+    technologies: [
+      "HTML",
+      "Tailwind CSS",
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Redux",
+    ],
   },
   {
     title: "Sudoku Solver",
     image: project2,
-    reference:"https://www.google.com",                //to be updated
-    description:                                        //to be updated
+    reference: "https://www.google.com", //to be updated
+    //to be updated
+    description:
       "A platsofm that solves the sudoku using backtracking and greedy algorithm",
-    technologies: ["HTML", "CSS", "React", "Javascript"],                //to be updated
+    technologies: ["HTML", "CSS", "React", "Javascript"], //to be updated
   },
   {
-    title: "Video Streaming Platform",                        
+    title: "Video Streaming Platform",
     image: project3,
-    reference:"https://www.google.com",                //to be updated
+    reference: "https://www.google.com", //to be updated
     description:
       "It is platform where users can upload the videos and can stream it later. To decrease the loading time, video is fragmented into small chunks so that small chunks will load one after the other instead of all at once.",
-    technologies: ["React", "express", "ffmpeg"],     //db to be added
+    technologies: ["React", "express", "ffmpeg"], //db to be added
   },
-  
+
+  // {
+  //   title: "blogging Website",
+  //   image: project4, //to be updated
+  //   reference: "https://www.google.com", //to be updated
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"], //to be updated
+  // },
+
   {
-    title: "blogging Website",                         
-    image: project4,                                   //to be updated
-    reference:"https://www.google.com",                //to be updated
+    title: "URL shortner",
+    image: shorturl, //to be updated
+    reference: "https://url-shortner-pi-eight.vercel.app/", //to be updated
+    //to be updated
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],                //to be updated
-  },
-  
-  {
-    title: "URL shortner",                             
-    image: project3,                                    //to be updated
-    reference:"https://www.google.com",                 //to be updated
-    description:                                        //to be updated
       "A platform where url are shortened using ------ algorithm, which is helpful in places where url lenght limit is less and also helps in reducing the file size where the file has laks of urls",
     technologies: ["HTML", "CSS", "React", "Tailwind CSS", "Javascript"], //to be updated
   },
